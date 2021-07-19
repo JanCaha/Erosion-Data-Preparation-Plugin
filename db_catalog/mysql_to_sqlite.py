68a67920bdbd4c00edd66e9d30ab8ad5147e3725
Jan Caha <jan.caha@outlook.com>
2021-07-19 23:26:47 +0200

    conversion script

---
+++ db_catalog/mysql_to_sqlite.py
@@ -0,0 +1,24 @@
+import os
+from pathlib import Path
+from osgeo import ogr, gdal
+
+conn_string = "MYSQL:testcatalog,user=root,password=root,host=localhost,port=3306"
+
+mysql_ds: ogr.DataSource = ogr.Open(conn_string)
+
+print(mysql_ds.GetLayerCount())
+
+
+driver: ogr.Driver = ogr.GetDriverByName("SQLite")
+
+path = Path(__file__).parent / "database.sqlite"
+
+ds: ogr.DataSource = driver.CreateDataSource(str(path))
+
+for i in range(mysql_ds.GetLayerCount()):
+
+    layer: ogr.Layer = mysql_ds.GetLayerByIndex(i)
+
+    print(F" - {i} : {layer.GetName()}")
+
+    ds.CopyLayer(layer, layer.GetName(), options=["FID=id"])
