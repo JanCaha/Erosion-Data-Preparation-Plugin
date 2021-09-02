cea9cefce11e298578a6d393e416cba5819e0f19
Jan Caha <jan.caha@outlook.com>
2021-09-02 16:27:20 +0200

    english is default

--- constants/__init__.py
+++ constants/__init__.py
@@ -1,2 +1,2 @@
-# from .texts_cze import TextConstantsCZ as TextConstants
-from .texts_en import TextConstantsEN as TextConstants
+from .texts_cze import TextConstantsCZ as TextConstants
+# from .texts_en import TextConstantsEN as TextConstants
