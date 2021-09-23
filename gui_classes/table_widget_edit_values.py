7e3e484a8f1a717f228524cb3df7c4f04a292d76
Jan Caha <jan.caha@outlook.com>
2021-09-24 00:07:10 +0200

    update widget to directly edit data and always build from scratch instead of relying on some kind of memorization

--- gui_classes/table_widget_edit_values.py
+++ gui_classes/table_widget_edit_values.py
@@ -2,13 +2,12 @@ from typing import Optional, NoReturn, Tuple
 
 from qgis.PyQt.QtWidgets import QTableWidget, QTableWidgetItem, QLineEdit, QHeaderView
 from qgis.PyQt import QtCore
-from qgis.PyQt.QtCore import QRegExp
+from qgis.PyQt.QtCore import QRegExp, QObject
 from qgis.PyQt.QtGui import QFont, QRegExpValidator, QColor
 
 from qgis.core import (QgsVectorLayer,
                        QgsVectorDataProvider,
-                       QgsFeature,
-                       NULL)
+                       QgsFeature)
 
 from ..constants import TextConstants
 from ..algorithms.utils import log
@@ -16,6 +15,21 @@ from .table_widget_item import TableItemNotEditable
 
 
 class TableWidgetEditNumericValues(QTableWidget):
+
+    data_layer: QgsVectorLayer
+    data_layer: QgsVectorDataProvider
+
+    fields_names_cols = {TextConstants.field_name_poly_id: 0,
+                         TextConstants.field_name_bulk_density: 1,
+                         TextConstants.field_name_corg: 2,
+                         TextConstants.field_name_init_moisture: 3,
+                         TextConstants.field_name_roughness: 4,
+                         TextConstants.field_name_canopy_cover: 5,
+                         TextConstants.field_name_skinfactor: 6,
+                         TextConstants.field_name_erodibility: 7,
+                         TextConstants.field_name_fid: 8}
+
+    cols = list(fields_names_cols.keys())
 
     def __init__(self,
                  parent=None):
@@ -52,106 +66,28 @@ class TableWidgetEditNumericValues(QTableWidget):
 
         return value
 
-    def update_values_in_layer(self, layer: QgsVectorLayer) -> NoReturn:
-
-        log("Updating from table")
-
-        layer_dp: QgsVectorDataProvider = layer.dataProvider()
-
-        fi_bulk_density = layer_dp.fieldNameIndex(TextConstants.field_name_bulk_density)
-        fi_corg = layer_dp.fieldNameIndex(TextConstants.field_name_corg)
-        fi_init_moisture = layer_dp.fieldNameIndex(TextConstants.field_name_init_moisture)
-        fi_roughness = layer_dp.fieldNameIndex(TextConstants.field_name_roughness)
-        fi_canopy_cover = layer_dp.fieldNameIndex(TextConstants.field_name_canopy_cover)
-        fi_skinfactor = layer_dp.fieldNameIndex(TextConstants.field_name_skinfactor)
-        fi_erodibility = layer_dp.fieldNameIndex(TextConstants.field_name_erodibility)
-
-        layer.startEditing()
-
-        for i in range(self.rowCount()):
-
-            log(self.item(i, 8).text())
-            fid = int(self.item(i, 8).text())
-
-            layer.changeAttributeValue(fid, fi_bulk_density, self.get_value(i, 1))
-            layer.changeAttributeValue(fid, fi_corg, self.get_value(i, 2))
-            layer.changeAttributeValue(fid, fi_init_moisture, self.get_value(i, 3))
-            layer.changeAttributeValue(fid, fi_roughness, self.get_value(i, 4))
-            layer.changeAttributeValue(fid, fi_canopy_cover, self.get_value(i, 5))
-            layer.changeAttributeValue(fid, fi_skinfactor, self.get_value(i, 6))
-            layer.changeAttributeValue(fid, fi_erodibility, self.get_value(i, 7))
-
-        layer.commitChanges()
-
-    def check_row_exist(self, feature: QgsFeature) -> Tuple[bool, Optional[int]]:
-
-        fid = str(feature.id())
-        poly_id = feature.attribute(TextConstants.field_name_poly_id)
-
-        exists = False
-        row_index = None
-
-        for i in range(self.rowCount()):
-
-            if fid == self.item(i, 8).text() and poly_id == self.item(i, 0).text():
-
-                exists = True
-                row_index = i
-
-                break
-
-        return exists, row_index
-
     def add_row(self, feature: QgsFeature):
 
-        row_exist, index = self.check_row_exist(feature)
+        row_to_put = self.rowCount()
+        self.insertRow(row_to_put)
 
-        if not row_exist:
-
-            row_to_put = self.rowCount()
-            self.insertRow(self.rowCount())
-
-            self.setItem(row_to_put, 0, TableItemNotEditable(feature.attribute(TextConstants.field_name_poly_id)))
-            self.setItem(row_to_put, 8, TableItemNotEditable(str(feature.id())))
-            self.setCellWidget(row_to_put, 1, self.add_cell_value(feature.attribute(TextConstants.field_name_bulk_density)))
-            self.setCellWidget(row_to_put, 2, self.add_cell_value(feature.attribute(TextConstants.field_name_corg)))
-            self.setCellWidget(row_to_put, 3, self.add_cell_value(feature.attribute(TextConstants.field_name_init_moisture)))
-            self.setCellWidget(row_to_put, 4, self.add_cell_value(feature.attribute(TextConstants.field_name_roughness)))
-            self.setCellWidget(row_to_put, 5, self.add_cell_value(feature.attribute(TextConstants.field_name_canopy_cover)))
-            self.setCellWidget(row_to_put, 6, self.add_cell_value(feature.attribute(TextConstants.field_name_skinfactor)))
-            self.setCellWidget(row_to_put, 7, self.add_cell_value(feature.attribute(TextConstants.field_name_erodibility)))
-
-        else:
-
-            if not self.get_value(index, 1):
-                self.setCellWidget(index, 1,
-                                   self.add_cell_value(feature.attribute(TextConstants.field_name_bulk_density)))
-
-            if not self.get_value(index, 2):
-                self.setCellWidget(index, 2,
-                                   self.add_cell_value(feature.attribute(TextConstants.field_name_corg)))
-
-            if not self.get_value(index, 3):
-                self.setCellWidget(index, 3,
-                                   self.add_cell_value(feature.attribute(TextConstants.field_name_init_moisture)))
-
-            if not self.get_value(index, 4):
-                self.setCellWidget(index, 4,
-                                   self.add_cell_value(feature.attribute(TextConstants.field_name_roughness)))
-
-            if not self.get_value(index, 5):
-                self.setCellWidget(index, 5,
-                                   self.add_cell_value(feature.attribute(TextConstants.field_name_canopy_cover)))
-
-            if not self.get_value(index, 6):
-                self.setCellWidget(index, 6,
-                                   self.add_cell_value(feature.attribute(TextConstants.field_name_skinfactor)))
-
-            if not self.get_value(index, 7):
-                self.setCellWidget(index, 7,
-                                   self.add_cell_value(feature.attribute(TextConstants.field_name_erodibility)))
+        self.setItem(row_to_put, 0, TableItemNotEditable(feature.attribute(TextConstants.field_name_poly_id)))
+        self.setItem(row_to_put, 8, TableItemNotEditable(str(feature.id())))
+        self.setCellWidget(row_to_put, 1, self.add_cell_value(feature.attribute(TextConstants.field_name_bulk_density)))
+        self.setCellWidget(row_to_put, 2, self.add_cell_value(feature.attribute(TextConstants.field_name_corg)))
+        self.setCellWidget(row_to_put, 3, self.add_cell_value(feature.attribute(TextConstants.field_name_init_moisture)))
+        self.setCellWidget(row_to_put, 4, self.add_cell_value(feature.attribute(TextConstants.field_name_roughness)))
+        self.setCellWidget(row_to_put, 5, self.add_cell_value(feature.attribute(TextConstants.field_name_canopy_cover)))
+        self.setCellWidget(row_to_put, 6, self.add_cell_value(feature.attribute(TextConstants.field_name_skinfactor)))
+        self.setCellWidget(row_to_put, 7, self.add_cell_value(feature.attribute(TextConstants.field_name_erodibility)))
 
     def add_data(self, layer: QgsVectorLayer):
+
+        self.data_layer = layer
+        self.data_layer_dp = layer.dataProvider()
+
+        self.clearContents()
+        self.setRowCount(0)
 
         for feature in layer.getFeatures():
 
@@ -167,6 +103,7 @@ class TableWidgetEditNumericValues(QTableWidget):
 
         validator = QRegExpValidator(QRegExp(r'^-?[0-9]+[\.,]?[0-9]{0,14}$'))
         cell_item.setValidator(validator)
+        cell_item.textChanged.connect(self.change_data_value)
         cell_item.returnPressed.connect(self.clear_focus)
 
         return cell_item
@@ -181,18 +118,57 @@ class TableWidgetEditNumericValues(QTableWidget):
 
         return item
 
+    def get_row_column(self, sender: QObject) -> tuple[int, int]:
+
+        row = 0
+        column = 0
+
+        for i in range(self.rowCount()):
+
+            for j in range(self.columnCount()):
+
+                if self.cellWidget(i, j) == sender:
+                    row = i
+                    column = j
+
+        return row, column
+
     def clear_focus(self):
 
         sender = self.sender()
 
+        row, column = self.get_row_column(self.sender())
+
         sender.clearFocus()
 
-        row = 0
-        for i in range(self.rowCount()):
-            if self.cellWidget(i, self.value_col) == sender:
-                row = i
+        next_column = column + 1
+        next_row = row
 
-        next_row = row + 1
+        if next_column >= self.columnCount()-1:
+            next_column = 1
+            next_row = next_row + 1
 
         if next_row <= self.rowCount() - 1:
-            self.cellWidget(next_row, self.value_col).setFocus()
+            self.cellWidget(next_row, next_column).setFocus()
+
+    def change_data_value(self):
+
+        row, column = self.get_row_column(self.sender())
+
+        self.change_data(int(self.item(row, self.fields_names_cols[TextConstants.field_name_fid]).text()),
+                         self.cols[column],
+                         self.get_value(row, column))
+
+    def change_data(self, feature_fid: int, column: str, value: float):
+
+        self.data_layer.startEditing()
+
+        field_index = self.data_layer_dp.fieldNameIndex(column)
+
+        feature = self.data_layer.getFeature(feature_fid)
+
+        feature.setAttribute(field_index, value)
+
+        self.data_layer.updateFeature(feature)
+
+        self.data_layer.commitChanges()
