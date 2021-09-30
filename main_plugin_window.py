061519c849c72bb6acd36fa3180f9ddfa96d8362
Jan Caha <jan.caha@outlook.com>
2021-09-30 18:19:48 +0200

    change way how "Particle size distribution" is handled

--- main_plugin_window.py
+++ main_plugin_window.py
@@ -3,8 +3,8 @@ from pathlib import Path
 from typing import Dict, List, Tuple
 import math
 
-from qgis.PyQt import uic
-from qgis.PyQt import QtWidgets
+from qgis.PyQt import uic, QtWidgets
+from qgis.PyQt.QtCore import QVariant
 
 from qgis.gui import QgsMapLayerComboBox, QgsFieldComboBox
 from qgis.core import (QgsMapLayerProxyModel,
@@ -24,7 +24,8 @@ from .algorithms.algs import (landuse_with_crops,
                               rename_field,
                               max_value_in_field,
                               add_row_without_geom,
-                              delete_features_with_values)
+                              delete_features_with_values,
+                              delete_fields)
 
 from .algorithms.algorithms_layers import (join_tables,
                                            intersect_dissolve,
@@ -80,6 +81,7 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
     # process parameters
     layer_soil: QgsVectorLayer = None
+    layer_soil_input: QgsVectorLayer = None
     layer_landuse: QgsVectorLayer = None
     layer_landuse_fields_backup: QgsFields = None
 
@@ -87,7 +89,6 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
     layer_landuse_interstep: QgsVectorLayer = None
 
     layer_intersected_dissolved: QgsVectorLayer = None
-    layer_intersected_dissolved_backup: QgsVectorLayer = None
     layer_raster_dtm: QgsRasterLayer = None
     layer_pour_points_rasterized: QgsRasterLayer = None
     layer_raster_rasterized: QgsRasterLayer = None
@@ -122,15 +123,15 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
     cb_addD90: QtWidgets.QCheckBox
 
     # widget 3
-    fcb_ftc: QgsFieldComboBox
-    fcb_mtc: QgsFieldComboBox
-    fcb_gtc: QgsFieldComboBox
-    fcb_fuc: QgsFieldComboBox
-    fcb_muc: QgsFieldComboBox
-    fcb_guc: QgsFieldComboBox
-    fcb_fsc: QgsFieldComboBox
-    fcb_msc: QgsFieldComboBox
-    fcb_gsc: QgsFieldComboBox
+    fcb_ftc: QtWidgets.QComboBox
+    fcb_mtc: QtWidgets.QComboBox
+    fcb_gtc: QtWidgets.QComboBox
+    fcb_fuc: QtWidgets.QComboBox
+    fcb_muc: QtWidgets.QComboBox
+    fcb_guc: QtWidgets.QComboBox
+    fcb_fsc: QtWidgets.QComboBox
+    fcb_msc: QtWidgets.QComboBox
+    fcb_gsc: QtWidgets.QComboBox
 
     # widget 4
     fcb_landuse: QgsFieldComboBox
@@ -292,16 +293,6 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
         self.label_FS.setText(TextConstants.label_FS)
         self.label_MS.setText(TextConstants.label_MS)
         self.label_GS.setText(TextConstants.label_GS)
-
-        self.fcb_ftc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_mtc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_gtc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_fuc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_muc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_guc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_fsc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_muc.setFilters(QgsFieldProxyModel.Numeric)
-        self.fcb_guc.setFilters(QgsFieldProxyModel.Numeric)
 
         # widget 4
         self.label_crop_field.setText(TextConstants.label_crop_field)
@@ -489,15 +480,9 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
         msg = ""
 
-        status = len(self.fcb_ftc.currentText()) > 0 and \
-                 len(self.fcb_mtc.currentText()) > 0 and \
-                 len(self.fcb_gtc.currentText()) > 0 and \
-                 len(self.fcb_fuc.currentText()) > 0 and \
+        status = len(self.fcb_mtc.currentText()) > 0 and \
                  len(self.fcb_muc.currentText()) > 0 and \
-                 len(self.fcb_guc.currentText()) > 0 and \
-                 len(self.fcb_fsc.currentText()) > 0 and \
-                 len(self.fcb_msc.currentText()) > 0 and \
-                 len(self.fcb_gsc.currentText()) > 0
+                 len(self.fcb_msc.currentText()) > 0
 
         if not status:
             msg = TextConstants.msg_select_all_fields
@@ -572,12 +557,19 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
             fields = self.layer_soil.fields()
 
+            field_names = [""]
+
             fields_to_remove = ["fid", "objectid", "shape_length", "shape_area"]
 
             for field_to_remove in fields_to_remove:
                 index = fields.lookupField(field_to_remove)
                 if index != -1:
                     fields.remove(index)
+
+            for field in fields:
+                if field.name() not in fields_to_remove:
+                    if field.type() == QVariant.Int or field.type() == QVariant.Double:
+                        field_names.append(field.name())
 
             if not self.field_soilid_cb.currentText():
                 self.field_soilid_cb.setFields(fields)
@@ -586,59 +578,23 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
                 self.field_ka5_cb.setFields(fields)
                 self.field_ka5_cb.setField("ka5")
 
-            self.fcb_ftc.setFields(fields)
-            if "ka5_ft" in fields.names():
-                self.fcb_ftc.setField("ka5_ft")
-            elif "FT" in fields.names():
-                self.fcb_ftc.setField("FT")
+            self.fcb_ftc.addItems(field_names)
 
-            self.fcb_mtc.setFields(fields)
-            if "ka5_mt" in fields.names():
-                self.fcb_mtc.setField("ka5_mt")
-            elif "MT" in fields.names():
-                self.fcb_mtc.setField("MT")
+            self.fcb_mtc.addItems(field_names)
 
-            self.fcb_gtc.setFields(fields)
-            if "ka5_gt" in fields.names():
-                self.fcb_gtc.setField("ka5_gt")
-            elif "GT" in fields.names():
-                self.fcb_gtc.setField("GT")
+            self.fcb_gtc.addItems(field_names)
 
-            self.fcb_fuc.setFields(fields)
-            if "ka5_fu" in fields.names():
-                self.fcb_fuc.setField("ka5_fu")
-            elif "FU" in fields.names():
-                self.fcb_fuc.setField("FU")
+            self.fcb_fuc.addItems(field_names)
 
-            self.fcb_muc.setFields(fields)
-            if "ka5_mu" in fields.names():
-                self.fcb_muc.setField("ka5_mu")
-            elif "MU" in fields.names():
-                self.fcb_muc.setField("MU")
+            self.fcb_muc.addItems(field_names)
 
-            self.fcb_guc.setFields(fields)
-            if "ka5_gu" in fields.names():
-                self.fcb_guc.setField("ka5_gu")
-            elif "GU" in fields.names():
-                self.fcb_guc.setField("GU")
+            self.fcb_guc.addItems(field_names)
 
-            self.fcb_fsc.setFields(fields)
-            if "ka5_fs" in fields.names():
-                self.fcb_fsc.setField("ka5_fs")
-            elif "FS" in fields.names():
-                self.fcb_fsc.setField("FS")
+            self.fcb_fsc.addItems(field_names)
 
-            self.fcb_msc.setFields(fields)
-            if "ka5_ms" in fields.names():
-                self.fcb_msc.setField("ka5_ms")
-            elif "MS" in fields.names():
-                self.fcb_msc.setField("MS")
+            self.fcb_msc.addItems(field_names)
 
-            self.fcb_gsc.setFields(fields)
-            if "ka5_gs" in fields.names():
-                self.fcb_gsc.setField("ka5_gs")
-            elif "GS" in fields.names():
-                self.fcb_gsc.setField("GS")
+            self.fcb_gsc.addItems(field_names)
 
     def update_value_layers(self):
 
@@ -876,8 +832,9 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
                 ok, msg = self.validate_widget_0()
 
                 if ok:
-                    self.layer_soil = copy_layer_fix_geoms(self.layer_soil_cb.currentLayer(),
-                                                           TextConstants.layer_soil)
+                    self.layer_soil_input = copy_layer_fix_geoms(self.layer_soil_cb.currentLayer(),
+                                                                 TextConstants.layer_soil)
+                    self.layer_soil = self.layer_soil_input
 
                     self.layer_landuse = copy_layer_fix_geoms(self.layer_landuse_cb.currentLayer(),
                                                               TextConstants.layer_landuse)
@@ -888,62 +845,158 @@ class MainPluginDialog(QtWidgets.QDialog, FORM_CLASS):
 
                 ok, msg = self.validate_widget_1()
 
-                rename_field(self.layer_soil,
-                             self.field_soilid_cb.currentText(),
-                             TextConstants.field_name_soil_id)
-
             if i == 2:
+
+                self.layer_soil = self.layer_soil_input.clone()
 
                 ok, msg = self.validate_widget_2()
 
                 if ok:
 
+                    rename_field(self.layer_soil,
+                                 self.field_soilid_cb.currentText(),
+                                 TextConstants.field_name_soil_id)
+
+                    self.progressBar.setMaximum(4)
+                    self.progressBar.setValue(1)
+
+                    # this three columns exists so rename them
+                    rename_field(self.layer_soil,
+                                 self.fcb_mtc.currentText(),
+                                 TextConstants.field_name_MT)
+
+                    rename_field(self.layer_soil,
+                                 self.fcb_muc.currentText(),
+                                 TextConstants.field_name_MU)
+
+                    rename_field(self.layer_soil,
+                                 self.fcb_msc.currentText(),
+                                 TextConstants.field_name_MS)
+
+                    self.progressBar.setValue(2)
+
+                    # these six columns may or may not be set, so either rename or create with 0 value
+
+                    if 0 < len(self.fcb_ftc.currentText()):
+
+                        if TextConstants.field_name_FT != self.fcb_ftc.currentText():
+
+                            delete_fields(self.layer_soil,
+                                          TextConstants.field_name_FT)
+
+                            rename_field(self.layer_soil,
+                                         self.fcb_ftc.currentText(),
+                                         TextConstants.field_name_FT)
+
+                    else:
+
+                        add_field_with_constant_value(self.layer_soil,
+                                                      TextConstants.field_name_FT,
+                                                      0)
+
+                    if 0 < len(self.fcb_gtc.currentText()):
+
+                        if TextConstants.field_name_GT != self.fcb_gtc.currentText():
+
+                            delete_fields(self.layer_soil,
+                                          TextConstants.field_name_GT)
+
+                            rename_field(self.layer_soil,
+                                         self.fcb_gtc.currentText(),
+                                         TextConstants.field_name_GT)
+
+                    else:
+
+                        add_field_with_constant_value(self.layer_soil,
+                                                      TextConstants.field_name_GT,
+                                                      0)
+
+                    if 0 < len(self.fcb_fuc.currentText()):
+
+                        if TextConstants.field_name_FU != self.fcb_fuc.currentText():
+
+                            delete_fields(self.layer_soil,
+                                          TextConstants.field_name_FU)
+
+                            rename_field(self.layer_soil,
+                                         self.fcb_fuc.currentText(),
+                                         TextConstants.field_name_FU)
+
+                    else:
+
+                        add_field_with_constant_value(self.layer_soil,
+                                                      TextConstants.field_name_FU,
+                                                      0)
+
+                    if 0 < len(self.fcb_guc.currentText()):
+
+                        if TextConstants.field_name_GU != self.fcb_guc.currentText():
+
+                            delete_fields(self.layer_soil,
+                                          TextConstants.field_name_GU)
+
+                            rename_field(self.layer_soil,
+                                         self.fcb_guc.currentText(),
+                                         TextConstants.field_name_GU)
+                    else:
+
+                        add_field_with_constant_value(self.layer_soil,
+                                                      TextConstants.field_name_GU,
+                                                      0)
+
+                    if 0 < len(self.fcb_fsc.currentText()):
+
+                        if TextConstants.field_name_FS != self.fcb_fsc.currentText():
+
+                            delete_fields(self.layer_soil,
+                                          TextConstants.field_name_FS)
+
+                            rename_field(self.layer_soil,
+                                         self.fcb_fsc.currentText(),
+                                         TextConstants.field_name_FS)
+                    else:
+
+                        add_field_with_constant_value(self.layer_soil,
+                                                      TextConstants.field_name_FS,
+                                                      0)
+
+                    if 0 < len(self.fcb_gsc.currentText()):
+
+                        if TextConstants.field_name_GS != self.fcb_gsc.currentText():
+
+                            delete_fields(self.layer_soil,
+                                          TextConstants.field_name_GS)
+
+                            rename_field(self.layer_soil,
+                                         self.fcb_gsc.currentText(),
+                                         TextConstants.field_name_GS)
+
+                    else:
+
+                        add_field_with_constant_value(self.layer_soil,
+                                                      TextConstants.field_name_GS,
+                                                      0)
+
+                    self.progressBar.setValue(3)
+
+                    add_maplayer_to_project(self.layer_soil)
+
                     ok, msg = classify_KA5(self.layer_soil,
-                                           self.fcb_ftc.currentText(),
-                                           self.fcb_mtc.currentText(),
-                                           self.fcb_gtc.currentText(),
-                                           self.fcb_fuc.currentText(),
-                                           self.fcb_muc.currentText(),
-                                           self.fcb_guc.currentText(),
-                                           self.fcb_fsc.currentText(),
-                                           self.fcb_msc.currentText(),
-                                           self.fcb_gsc.currentText(),
+                                           TextConstants.field_name_FT,
+                                           TextConstants.field_name_MT,
+                                           TextConstants.field_name_GT,
+                                           TextConstants.field_name_FU,
+                                           TextConstants.field_name_MU,
+                                           TextConstants.field_name_GU,
+                                           TextConstants.field_name_FS,
+                                           TextConstants.field_name_MS,
+                                           TextConstants.field_name_GS,
                                            TextConstants.field_name_ka5_code,
                                            TextConstants.field_name_ka5_name,
                                            TextConstants.field_name_ka5_id,
                                            self.progressBar)
 
-                    self.progressBar.setMaximum(2)
-                    self.progressBar.setValue(1)
-
-                    rename_field(self.layer_soil,
-                                 self.fcb_ftc.currentText(),
-                                 TextConstants.field_name_FT)
-                    rename_field(self.layer_soil,
-                                 self.fcb_mtc.currentText(),
-                                 TextConstants.field_name_MT)
-                    rename_field(self.layer_soil,
-                                 self.fcb_gtc.currentText(),
-                                 TextConstants.field_name_GT)
-                    rename_field(self.layer_soil,
-                                 self.fcb_fuc.currentText(),
-                                 TextConstants.field_name_FU)
-                    rename_field(self.layer_soil,
-                                 self.fcb_muc.currentText(),
-                                 TextConstants.field_name_MU)
-                    rename_field(self.layer_soil,
-                                 self.fcb_guc.currentText(),
-                                 TextConstants.field_name_GU)
-                    rename_field(self.layer_soil,
-                                 self.fcb_fsc.currentText(),
-                                 TextConstants.field_name_FS)
-                    rename_field(self.layer_soil,
-                                 self.fcb_msc.currentText(),
-                                 TextConstants.field_name_MS)
-                    rename_field(self.layer_soil,
-                                 self.fcb_gsc.currentText(),
-                                 TextConstants.field_name_GS)
-                    self.progressBar.setValue(2)
+                    self.progressBar.setValue(4)
 
             if i == 3:
 
