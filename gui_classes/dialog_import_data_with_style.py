5d70ddabf971e47dd00c9443c4354a3afb4746ff
Jan Caha <jan.caha@outlook.com>
2021-11-08 18:35:19 +0100

    update dialog for data import

--- gui_classes/dialog_import_data_with_style.py
+++ gui_classes/dialog_import_data_with_style.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
 from qgis.PyQt import uic
-from qgis.PyQt.QtWidgets import QDialog, QLabel, QComboBox, QSlider
+from qgis.PyQt.QtWidgets import QDialog, QLabel, QComboBox, QLineEdit
 
-from qgis.gui import QgsFileWidget
+from qgis.gui import QgsFileWidget, QgsOpacityWidget
 from qgis.core import QgsRasterLayer
 
 from ..constants import TextConstants
@@ -18,7 +18,11 @@ class DialogLoadResult(QDialog):
     label_style: QLabel
     label_input_data: QLabel
     label_opacity: QLabel
-    horizontalSlider_opacity: QSlider
+    label_layer_name: QLabel
+
+    lineEdit_layer_name: QLineEdit
+
+    horizontalSlider_opacity: QgsOpacityWidget
 
     styles = {TextConstants.dialog_load_data_7_cat_tha: "7_cat_t_ha.qml",
               TextConstants.dialog_load_data_7_cat_kgm2: "7_cat_kg_m2.qml",
@@ -35,11 +39,16 @@ class DialogLoadResult(QDialog):
 
         self.setWindowTitle(TextConstants.dialog_load_data_title)
 
+        self.horizontalSlider_opacity.setOpacity(0.5)
+
         self.label_input_data.setText(TextConstants.dialog_load_data_layer)
         self.label_style.setText(TextConstants.dialog_load_data_style)
         self.label_opacity.setText(TextConstants.dialog_load_data_opacity)
+        self.label_layer_name.setText(TextConstants.dialog_load_data_layer_name)
 
-        self.mQgsFileWidget.setFilter("Arc/Info ASCII Grid (*.asc);;GeoTiff files (*.tif)")
+        self.lineEdit_layer_name.setText("sedbudget")
+
+        self.mQgsFileWidget.setFilter("Sedbudget Arc/Info ASCII Grid (sedbudget.asc);;Arc/Info ASCII Grid (*.asc);;")
         # self.mQgsFileWidget.setFilter("GeoTiff files (*.tif);;All files (*.*)")
 
         self.comboBox_style.addItems(list(self.styles.keys()))
@@ -55,15 +64,15 @@ class DialogLoadResult(QDialog):
 
     def get_opacity(self) -> float:
 
-        return self.horizontalSlider_opacity.value() / 100
+        return self.horizontalSlider_opacity.opacity()
 
     def load_result_data_with_style(self):
 
         file_path = self.mQgsFileWidget.filePath()
 
-        layer_name = Path(file_path).stem
-
         if file_path:
+
+            layer_name = self.lineEdit_layer_name.text()
 
             raster = QgsRasterLayer(file_path, layer_name)
 
